2b19f86f462380009af7945f937166e191d71344
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-08-23 14:52:15 +0300

    feat: creating the security of the project

---
+++ src/security/errorHandler.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from './Errors';
+
+export const errorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    return res.status(err.status).json({ message: err.message });
+  }
+  console.error(err);
+  return res.status(500).json({ message: 'Internal server error' });
+};
