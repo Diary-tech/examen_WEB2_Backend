ced6093115f974adbb0169b24f8d20f93b6eabbb
Diary HERINANDRASANA <hei.diary.4@gmail.com>
2026-08-23 10:09:31 +0300

    feat: adding the result interface that while be resend to the students (RG-12)

--- src/model/Attempt.ts
+++ src/model/Attempt.ts
@@ -9,3 +9,13 @@ export interface Attempt {
 export interface SubmitAttemptInput {
   answers: { questionId: number; choiceId: number }[];
 }
+
+export interface AttemptResult {
+  attemptId: number;
+  examId: number;
+  examTitle: string;
+  score: number;
+  maxScore: number;
+  submittedAt: Date;
+  corrections: QuestionCorrection[];
+}
